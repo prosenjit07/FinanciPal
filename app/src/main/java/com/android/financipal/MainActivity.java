6c96201e9be5dc4a9c739600f837b325c9331db3
Prosenjit Biswas <98583038+prosenjit07@users.noreply.github.com>
2023-10-28 13:08:16 +0600

    Relative Layout Add Home Page layout done

--- app/src/main/java/com/android/financipal/MainActivity.java
+++ app/src/main/java/com/android/financipal/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.financipal.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
